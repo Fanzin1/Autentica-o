c2961c00f91a3e06c7dc23bc7940c304f75642da
Fanzin1 <ricardofanzin@hotmail.com>
2020-12-23 22:15:31 -0300

    Autenticação e Segurança - 2

---
+++ app.js
@@ -0,0 +1,82 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema({
+    user: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+});
+
+app.get("/register", function(req, res){
+    res.render("register.ejs");
+});
+
+app.post("/register", function(req, res){
+    const user = req.body.username;
+    const password = req.body.password;
+
+    const newUser = new User({
+        user: user,
+        password: password
+    });
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    });
+});
+
+app.get("/login", function(req, res){
+    res.render("login.ejs");
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({user: username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+});
+
+
+
+app.listen(3000, function () {
+    console.log("Server listen on port 3000");
+});
