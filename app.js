aa7c32cd0c7305bf3a759210d89272356ad79d55
Fanzin1 <ricardofanzin@hotmail.com>
2020-12-23 23:00:05 -0300

    Adicição de HASH MD5

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
-
+const md5 = require('md5');
 
 const app = express();
 
@@ -22,7 +21,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -36,7 +34,7 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
     const user = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     const newUser = new User({
         user: user,
@@ -59,7 +57,7 @@ app.get("/login", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({user: username}, function(err, foundUser){
         if(err){
