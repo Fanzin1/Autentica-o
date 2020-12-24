6d61955c284f4d5fac60673cf18488461eaf1ae0
Fanzin1 <ricardofanzin@hotmail.com>
2020-12-24 11:55:42 -0300

    Remoção do MD5 e adição do bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -34,11 +35,13 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
     const user = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        
     const newUser = new User({
         user: user,
-        password: password
+        password: hash
     });
 
     newUser.save(function(err){
@@ -48,7 +51,11 @@ app.post("/register", function(req, res){
         else{
             res.render("secrets");
         }
+    });       
     });
+
+
+
 });
 
 app.get("/login", function(req, res){
@@ -57,7 +64,8 @@ app.get("/login", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
+
 
     User.findOne({user: username}, function(err, foundUser){
         if(err){
@@ -65,15 +73,15 @@ app.post("/login", function(req, res){
         }
         else{
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                });            
             }
         }
-    })
+    });
 });
-
-
 
 app.listen(3000, function () {
     console.log("Server listen on port 3000");
